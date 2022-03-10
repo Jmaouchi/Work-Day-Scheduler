b8d31e914b742875fd28fd2c4b9c31acc9091c74
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-10 00:54:11 -0600

    start making progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,14 @@ var localTime = document.getElementById('currentDay');
 var currentTime =  moment().format('d-MM-YYYY  h:mm:ss a');
 
 localTime.innerHTML = currentTime;
+
+// Change the color once the local hour will be equal to are hour data-time value;
+var data = $('#hours').data('time');
+console.log("The value is = " + data);
+var hour = moment().hours(); 
+for (i=0 ; i<row.length ; i++){
+if(hour === data){
+  var timer = $('#timer');
+  $(timer).css('color', 'red');
+}
+}
