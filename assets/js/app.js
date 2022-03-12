0a02eebc419e76a2fa22bf414138b49b0dc1a886
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-12 01:06:37 -0600

    added a each loop through all the elements that has data-time and matched it with the hour(),

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,38 @@
-var row = $('.row');
+var container = document.querySelectorAll('.container');
+var inputCol = $('input');
 var array = []
+var attrSet = 23;
 
 // set local time using moment.js
 var localTime = document.getElementById('currentDay');
-var currentTime =  moment().format('d-MM-YYYY  h:mm:ss a');
+var currentTime =  moment().format('MM-DD-YYYY  h:mm:ss a');
 
 localTime.innerHTML = currentTime;
 
 // Change the color once the local hour will be equal to are hour data-time value;
-var data = $('#hours').data('time');
-console.log("The value is = " + data);
-var hour = moment().hours(); 
-for (i=0 ; i<row.length ; i++){
-if(hour === data){
-  var timer = $('#timer');
-  $(timer).css('color', 'red');
-}
-}
+var currentHour = moment().hours(); 
+var currentDay = moment().date();
+console.log(`current day: ${currentDay}`);
+console.log(`current_hour: ${currentHour}`);
+
+  $(".time-block").each(function() {
+  var timeBlock = parseInt($(this).attr('data-time')).valueOf();
+    console.log(`time_block: ${timeBlock}`);
+    var currentInputElement = $(this).children()[1];;
+    console.log(`children: ${currentInputElement}`);
+
+  if(currentHour === timeBlock ){
+
+    currentInputElement.classList.add("present");
+
+  }else if(currentHour > timeBlock){
+
+    currentInputElement.classList.add("past");
+   
+  }else if (currentHour < timeBlock){
+
+    currentInputElement.classList.add("future");
+
+      }
+    })
+  
