985684724b113335f7eb1fb6b6740bad89930776
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-12 21:40:14 -0600

    added first input value to local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var container = document.querySelectorAll('.container');
 var inputCol = $('input');
+var textEvent = $('#one');
 var array = []
 var attrSet = 23;
 
@@ -12,10 +13,11 @@ localTime.innerHTML = currentTime;
 // Change the color once the local hour will be equal to are hour data-time value;
 var currentHour = moment().hours(); 
 var currentDay = moment().date();
-console.log(`current day: ${currentDay}`);
-console.log(`current_hour: ${currentHour}`);
+  console.log(`current day: ${currentDay}`);
+  console.log(`current_hour: ${currentHour}`);
 
-  $(".time-block").each(function() {
+
+$(".time-block").each(function() {
   var timeBlock = parseInt($(this).attr('data-time')).valueOf();
     console.log(`time_block: ${timeBlock}`);
     var currentInputElement = $(this).children()[1];;
@@ -35,4 +37,34 @@ console.log(`current_hour: ${currentHour}`);
 
       }
     })
-  
+
+    //first saved input to localStorage
+    var getValueOne = function(){
+    var valueLs =  localStorage.getItem('one');
+      console.log(` valusLS: ${valueLs}`);
+      document.getElementById('one').textContent = valueLs;
+    }
+
+    getValueOne();
+
+    function setValueOne (){ 
+      // get the value of the textarea
+      var value = $(textEvent).val();
+      console.log(`value: ${value}`);
+
+      localStorage.setItem('one', value);
+    }
+
+
+    //second saved input to localStorage
+
+
+    $('.saveBtnOne').on('click', setValueOne)
+    // $('.saveBtnTwo').on('click', setValueTwo)
+    // $('.saveBtnThree').on('click', setValueTwo)
+    // $('.saveBtnFour').on('click', setValueOne)
+    // $('.saveBtnFive').on('click', setValueOne)
+    // $('.saveBtnSix').on('click', setValueOne)
+    // $('.saveBtnSeven').on('click', setValueOne)
+    // $('.saveBtnEight').on('click', setValueOne)
+    // $('.saveBtnNine').on('click', setValueOne)
